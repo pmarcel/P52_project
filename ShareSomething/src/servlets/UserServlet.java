8124d0aa29bfb33c32f338406b392f1f2e2a7473
pmarcel <pierre.marcel@etu-unistra.fr>
2015-01-23 10:21:09 +0100

    Mapping userServlet

---
+++ ShareSomething/src/servlets/UserServlet.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserManager
+ */
+@WebServlet(
+		description = "gestion des connexion/inscriptions", 
+		urlPatterns = { 
+				"/login", 
+				"/register"
+		})
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public UserServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+}	
