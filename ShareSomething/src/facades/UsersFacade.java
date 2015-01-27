a713e08439eca2932dfe58a41eb232f51cb46450
Eli Griv <eurobase@live.fr>
2015-01-27 20:30:00 +0100

    Ajouté la création des users

--- ShareSomething/src/facades/UsersFacade.java
+++ ShareSomething/src/facades/UsersFacade.java
@@ -30,6 +30,39 @@ public class UsersFacade extends BaseFacade {
 		return res;
 	}
 	
+	public static User create(String login, String password) {
+		EntityManager m = getEM();
+		Query q = null;
+		User res = null;
+		
+		try {
+			q = m.createQuery("SELECT COUNT(e FROM User e WHERE login LIKE " + login);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			int nb = 1;
+			try {
+				nb = (int) q.getSingleResult();
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+			
+			if (nb == 0) {
+				try {
+					res = new User();
+					res.setLogin(login);
+					res.setPassword(password);
+					
+					m.persist(res);
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		}
+
+		return res;
+	}
+	
 	public static User read(int id) {
 		EntityManager m = getEM();
 		Query q = null;
@@ -51,18 +84,24 @@ public class UsersFacade extends BaseFacade {
 		return res;
 	}
 	
-	public static boolean CheckLogin(String login, String password) {
+	/**
+	 * Renvoie Le user si la connexion s'est bien passée
+	 * @param login
+	 * @param password
+	 * @return Le user ou null
+	 */
+	public static User CheckLogin(String login, String password) {
 		EntityManager m = getEM();
 		Query q = null;
-		boolean res = false;
+		User res = null;
 		
 		try {
-			q = m.createQuery("SELECT COUNT(e) FROM Users e WHERE login LIKE '" + login + "' AND password = '" + password + "'");
+			q = m.createQuery("SELECT e FROM Users e WHERE login LIKE '" + login + "' AND password = '" + password + "'");
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		} finally {
 			try {
-				res = ((int) q.getSingleResult()) == 1 ? true : false;		// Si plus d'un résultat (ou zéro), login failed
+				res = (User) q.getSingleResult();		// Si plus d'un résultat (ou zéro), login failed
 			} catch (Exception e) {
 				System.err.println(e.getMessage());
 			}
